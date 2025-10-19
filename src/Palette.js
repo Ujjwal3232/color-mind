e4e78b34d37ea4c3404bdfae0fa4cc1021ee6bc2
Ujjwal3232 <ujjwal.tiwari.cs22@ggits.net>
2025-10-19 18:16:14 +0530

    Added palette footer

--- src/Palette.js
+++ src/Palette.js
@@ -24,10 +24,10 @@ class Palette extends Component {
     this.setState({ format : val});
   }
   render() {
-    const { colors } = this.props.palette;
+    const { colors,paletteName,emoji} = this.props.palette;
     const { level ,format } = this.state;
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} key={color.id}/>
     ));
     return (
       <div className="Palette">
@@ -42,6 +42,10 @@ class Palette extends Component {
           {/* Bunch of colors boxes */}
         </div>
         {/* footer */}
+        <footer className="Palette-footer">
+         {paletteName}
+         <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
