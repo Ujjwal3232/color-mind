ee832110e64adccfd954df9ad950c8f1eb536d03
Ujjwal3232 <ujjwal.tiwari.cs22@ggits.net>
2025-10-29 21:08:28 +0530

    refactor: moved all inline component styles into separate JSS files under /styles

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -3,35 +3,11 @@ import ColorBox from "./ColorBox";
 import PaletteFooter from './PaletteFooter';
 import { withStyles } from '@mui/styles';
 import "rc-slider/assets/index.css";
-import "./Palette.css";
 import Navbar from "./Navbar";
+import styles from "./styles/PaletteStyles"
 
-const styles={
-   Palette: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-  },
 
-  PaletteColors: {
-    height: "90%",
-  },
 
-  PaletteFooter: {
-    backgroundColor: "white",
-    height: "5vh",
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    fontWeight: "bold",
-  },
-
-  emoji: {
-    fontSize: "1.5rem",
-    margin: "0 1rem",
-  },
-  
-}
 
 class Palette extends Component {
   constructor(props) {
