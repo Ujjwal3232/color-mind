9a65fc0d52cdd65c3678808e18001f5196ae093d
Ujjwal3232 <ujjwal.tiwari.cs22@ggits.net>
2025-11-09 16:37:33 +0530

    Adding Color Picker & Buttons to Sidebar

--- src/NewPaletteForm.jsx
+++ src/NewPaletteForm.jsx
@@ -1,10 +1,156 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import {ChromePicker} from 'react-color';
 
+
+const drawerWidth = 500;
+
+const Main = styled('main', {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+// 🌈 Converted functional logic to class-based
 export default class NewPaletteForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      direction: 'ltr', // manually manage theme direction
+    };
+  }
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
+    const { open, direction } = this.state;
+    const theme = createTheme({ direction });
+
     return (
-      <div><h1>New Palette Form! </h1></div>
-    )
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <AppBar position="fixed" open={open}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={this.handleDrawerOpen}
+                edge="start"
+                sx={{ mr: 2, ...(open && { display: 'none' }) }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap component="div">
+                New Palette Form
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{
+              width: drawerWidth,
+              flexShrink: 0,
+              '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+              },
+            }}
+            variant="persistent"
+            anchor="left"
+            open={open}
+          >
+            <DrawerHeader>
+              <IconButton onClick={this.handleDrawerClose}>
+                <ChevronLeftIcon /> 
+              </IconButton>
+            </DrawerHeader>
+            <Divider />
+            <Typography variant='h4'>
+             Design Your Palette
+            </Typography>
+            <div>
+            <Button variant='contained' color='secondary'>
+              Clear Palette
+            </Button>
+            <Button variant='contained' color='primary'>
+              Random Color
+            </Button>
+            </div>
+            <ChromePicker 
+               color={'purple'}
+               onChangeComplete={(newColor)=>console.log(newColor)
+
+               }
+            />
+            <Button variant='contained' color='primary'>
+              Add Color
+            </Button>
+          </Drawer>
+
+          
+
+          <Main open={open}>
+            <DrawerHeader />
+            
+          </Main>
+        </Box>
+      </ThemeProvider>
+    );
   }
 }
-
